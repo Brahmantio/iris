b59acf5141cb98f607c5363a24cade91bd41aef8
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-08-03 17:05:34 +0700

    Update porto1.py

--- porto1.py
+++ porto1.py
@@ -233,14 +233,13 @@ def house():
         if st.button('Predict Now!'):
             #model_loc = '/mount/src/course/modeldqlab.pkl'
          with open('tesis.pkl','rb') as file:
-                st.write(features)
-            model = pickle.load(file)
-            prediction1 = model.predict(features)
-            prediction = np.expm1(prediction1)
+                 st.write(features)
+                 model = pickle.load(file)
+                 prediction1 = model.predict(features)
+                 prediction = np.expm1(prediction1)
          with st.spinner('Wait for it...'):
-            time.sleep(4)
-            st.success(f"Hasil prediksi: Rp{prediction[0]:,.2f}")
-     
+                 time.sleep(4)
+                 st.success(f"Hasil prediksi: Rp{prediction[0]:,.2f}")
          with tab2:
             st.header("How to use this application")
             st.write("1. Apabila ingin  memprediksi menggunakan file, pastikan file tersebut dalam format .csv dan seluruh atribut sama")
