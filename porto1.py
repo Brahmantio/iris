2b9f5314c5e5de50dc79ca63f78cb93afaa478a4
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-08-12 16:32:17 +0700

    Update porto1.py

--- porto1.py
+++ porto1.py
@@ -218,7 +218,7 @@ def palm():
         st.write("Submit gambar kelapa sawit yang anda miliki, kemudian model akan mengklasifikasikan gambar antara Belum Matang, Matang, atau Terlalu Matang")
 
         # Load model
-        model = load_model("sawit_model.keras")
+        model = load_model("sawit_model.keras", compile=False)
 
         # Upload file
         uploaded_file = st.file_uploader("Upload Gambar", type=["jpg", "png", "jpeg"])
