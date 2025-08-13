000e1e0a6b7809476b908bcad61e369258379d3f
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-08-13 19:31:11 +0700

    Update porto1.py

--- porto1.py
+++ porto1.py
@@ -14,7 +14,7 @@ Hello my name is [Bramantio](https://www.linkedin.com/in/brahmantio-w/) and I am
 \n With a strong enthusiasm for data analysis, I enjoy transforming raw information into meaningful stories that drive better decision-making.
 
 """)
-add_selectitem = st.sidebar.selectbox("Want to open about?", ("House prediction","Iris species", "Heart disease"))
+add_selectitem = st.sidebar.selectbox("Want to open about?", ("House prediction","Palm oil classification","Iris species", "Heart disease"))
 
 def house():
         st.write("""
@@ -211,6 +211,47 @@ def house():
                         time.sleep(4)
                         st.success(f"Hasil prediksi: Rp{prediction[0]:,.2f}")
                         
+def palm():
+        import tensorflow as tf
+        from tensorflow.keras.models import load_model
+        from tensorflow.keras.preprocessing import image
+        from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+        
+        st.header("Implementasi Convolutional Neural Network untuk Identifikasi Tingkat Kematangan Buah Kelapa Sawit")
+        st.write("Dataset yang digunakan berasal dari [kaggle](https://www.kaggle.com/datasets/ramadanizikri112/ripeness-of-oil-palm-fruit)")
+        st.write("Submit gambar kelapa sawit yang anda miliki, kemudian model akan mengklasifikasikan gambar antara Belum Matang, Matang, atau Terlalu Matang")
+
+        # Load model
+        model = load_model("sawit_model.keras",compile=False)
+
+        # Upload file
+        uploaded_file = st.file_uploader("Upload Gambar", type=["jpg", "png", "jpeg"])
+
+        if uploaded_file is not None:
+                    # tampilkan
+                    img_pil = Image.open(uploaded_file).convert("RGB")
+                    st.image(img_pil, caption="Gambar yang diunggah", use_column_width=True)
+
+                    # resize & preprocess
+                    img_resized = img_pil.resize((224, 224))
+                    img_array = image.img_to_array(img_resized)
+                    img_array = np.expand_dims(img_array, axis=0)
+                    img_array = preprocess_input(img_array)
+
+
+                    # Prediksi
+                    preds = model.predict(img_array)
+                    predicted_class = np.argmax(preds, axis=1)
+    
+                    # Mapping kelas ke label
+                    label_mapping = {
+                                0: "Belum Matang",
+                        1: "Matang",
+                        2: "Terlalu Matang"
+                        }
+                    label_prediksi = label_mapping[predicted_class[0]]
+
+                    st.write(f"Hasil Prediksi: {label_prediksi}")
 
 def iris():
     st.write("""
@@ -327,6 +368,8 @@ def heart():
          
 if add_selectitem == "House prediction":
     house()
+elif add_selectitem == "Palm oil classification":
+    palm()
 elif add_selectitem == "Iris species":
     iris()
 elif add_selectitem == "Heart disease":
