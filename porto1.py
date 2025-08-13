0910cfb8394990aae479d4748f2e05c3f881b59f
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-08-13 13:07:13 +0700

    Update porto1.py

--- porto1.py
+++ porto1.py
@@ -220,7 +220,7 @@ def palm():
         MODEL_PATH = os.path.join(os.getcwd(), "kelapasawit_model.h5")
 
         # Load model tanpa compile
-        model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+        model = tf.keras.models.load_model(MODEL_PATH)
 
         st.title("Implementasi CNN - Kematangan Kelapa Sawit")
 
